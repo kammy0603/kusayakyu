59900e3c5403927e710b9460f1348d7d9519eb9a
kammy0603 <wingzero0127@gmail.com>
2021-03-14 19:57:27 +0900

    データベース修正

--- db/migrate/20210207074448_create_teams.rb
+++ db/migrate/20210207074448_create_teams.rb
@@ -2,12 +2,12 @@ class CreateTeams < ActiveRecord::Migration[6.0]
   def change
     create_table :teams do |t|
       t.string  :teamname,       null: false
-      t.integer :prefecture_id,     null: false
+      t.integer :prefecture_id,  null: false
       t.integer :year,           null: false
-      t.integer :age_id,            null: false
-      t.integer :frequency_id,      null: false
-      t.integer :level_id,          null: false
-      t.integer :offer_id,          null: false
+      t.integer :age_id,         null: false
+      t.integer :frequency_id,   null: false
+      t.integer :level_id,       null: false
+      t.integer :offer_id,       null: false
       t.string  :achievement,    null: false
       t.string  :location,       null: false
       t.timestamps
